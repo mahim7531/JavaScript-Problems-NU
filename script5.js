5ce029eb68d0ba44cf55247ecbfcda376011f0db
Md Tayam Hasan Mahim <mdtayamhasanmahim7531@gmail.com>
2026-04-23 22:13:22 +0600

    13th

---
+++ script5.js
@@ -0,0 +1,15 @@
+// Count Vowels in String
+function countVowels(str) {
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("Hello World")); // 3
